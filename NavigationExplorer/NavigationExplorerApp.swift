9e2a456947f491d4e5be6a00c1b07bc6deb23a22
carlynorama <carlynorama@gmail.com>
2022-09-30 14:47:48 -0700

    Initial Commit

---
+++ NavigationExplorer/NavigationExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExplorerApp.swift
+//  NavigationExplorer
+//
+//  Created by Labtanza on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
