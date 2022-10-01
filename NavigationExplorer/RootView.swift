57f6aa11161c9db0402af2f2b20d58bb827e23b9
carlynorama <carlynorama@gmail.com>
2022-09-30 18:49:24 -0700

    .id works

--- NavigationExplorer/RootView.swift
+++ NavigationExplorer/RootView.swift
@@ -10,13 +10,17 @@ import SwiftUI
 
 
 
-struct ContentView: View {
+struct RootView: View {
     @StateObject private var nav = NavigationManager()
+    
+    @State private var helloNewItem = false
     
     var body: some View {
         NavigationStack(path: $nav.visibilityStack) {
             VStack {
                 Button("Pile Banana and Strawberry", action: bananaAndStrawberry)
+                    .padding(40)
+                Button("Push Hello") { helloNewItem.toggle() }
                     .padding(40)
                 List(Fruit.fruitList) {
                     fruit in NavigationLink(value: NavigationManager.RouteItem.Fruit(fruit)) {
@@ -33,12 +37,14 @@ struct ContentView: View {
             .navigationDestination(for: NavigationManager.RouteItem.self) { route in
                 switch route {
                 case .Fruit(let fruit):
-                    FruitView(fruit: fruit).environmentObject(nav)
+                    FruitView(fruit: fruit)
+                        .id(fruit)
+                        .environmentObject(nav)
                     
                 case .Vehicle(let vehicle):
                     VehicleView(vehicle: vehicle).environmentObject(nav)
                 }
-            }
+            }.navigationDestination(isPresented: $helloNewItem, destination: { Text("Hello") })
         }
     }
     
@@ -50,6 +56,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootView()
     }
 }
